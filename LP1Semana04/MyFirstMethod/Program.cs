2cdd849354ec38c1ce9f37bcb2886aac1b60a7c2
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2022-03-17 15:19:26 +0000

    ex.6 V1

--- LP1Semana04/MyFirstMethod/Program.cs
+++ LP1Semana04/MyFirstMethod/Program.cs
@@ -3,32 +3,35 @@
 namespace NomeDoProjeto
 {
     /// <summary>
-    /// Programa que vai escrever na consola todos os numeros inteiros ate ao numero dito pelo user
+    /// Programa que vai escrever na consola todos os numeros inteiros de um numero ate outro ambos dados por ordem pelo user
     /// </summary>
     class Program
     {
         /// <summary>
-        /// Main, onde se vai chamar tudo
+        /// Vao ser pedidos dois numeros e executado CountToN 2 vezes
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string aux = Console.ReadLine();
-            int n = Convert.ToInt32(aux);
-            CountToN(n);
-            aux = Console.ReadLine();
-            n = Convert.ToInt32(aux);
-            CountToN(n);
+            for(int i = 0; i < 2; i++)
+            {
+                string aux = Console.ReadLine();
+                int n = Convert.ToInt32(aux);
+                aux = Console.ReadLine();
+                int n2 = Convert.ToInt32(aux);
+                CountToN(n1, n2);
+            }
         }
         /// <summary>
-        /// Recebe um numero e vai escrever um a um todos os inteiros des de 1 ate n
+        /// Recebe dois numeros e vai escrever um a um todos os inteiros des do primeiro ate ao segundo
         /// </summary>
-        /// <param name="n">Ultimo numero escrito</param>
-        private static void CountToN(int n)
+        /// <param name="n1">Primeiro numero a ser escrito</param>
+        /// <param name="n2">Ultimo numero a ser escrito</param>
+        private static void CountToN(int n1, int n2)
         {
-            for(int i = 1; i <= n; i++)
+            for(int i = n1; i <= n2; i++)
             {
-                Console.WriteLine(n);
+                Console.WriteLine(i);
             }
         }
 
