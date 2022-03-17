0e0c8c0c9f71d1348374cad2b2e24d2fdf4b75d9
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2022-03-17 14:51:13 +0000

    ex.1 V1

---
+++ LP1Semana04/MyFirstMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeDoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountTo10();
+            CountTo10();
+        }
+        private static void CountTo10 ()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }       
+
+    }
+}
