5db3373801e82b530ea4d3f45f9c3d2eee335e76
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2022-03-17 15:52:25 +0000

    ex.7 V1

---
+++ LP1Semana04/Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gibe num to fibo");
+            string aux = Console.ReadLine();
+            int n = ToInt34(aux)
+            Fibonacci(n)
+        }
+        private static int Fibonacci (int n)
+        {
+            ulong fibo;
+            if (n == 1 || n == 2)
+                fibo = 1;
+            else
+                fibo = fibo(n - 2) + fibo (n - 1);
+            return fibo;
+        }
+    }
+}
