ec88f918e338222598b046cb9ca94da32eac8b21
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2022-03-17 15:09:36 +0000

    ex.5 V1

--- LP1Semana04/MyFirstMethod/Program.cs
+++ LP1Semana04/MyFirstMethod/Program.cs
@@ -2,8 +2,15 @@
 
 namespace NomeDoProjeto
 {
+    /// <summary>
+    /// Programa que vai escrever na consola todos os numeros inteiros ate ao numero dito pelo user
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main, onde se vai chamar tudo
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string aux = Console.ReadLine();
@@ -13,7 +20,10 @@ namespace NomeDoProjeto
             n = Convert.ToInt32(aux);
             CountToN(n);
         }
-
+        /// <summary>
+        /// Recebe um numero e vai escrever um a um todos os inteiros des de 1 ate n
+        /// </summary>
+        /// <param name="n">Ultimo numero escrito</param>
         private static void CountToN(int n)
         {
             for(int i = 1; i <= n; i++)
