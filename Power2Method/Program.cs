f7993d798dc874490c1dbb7240498d9a41988c6c
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-23 17:28:00 +0000

    Add PowersOf2UntilN call twice

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -6,13 +6,20 @@ namespace Power2Method
     {
         static void Main(string[] args)
         {
+            PowersOf2UntilN(7);
+            PowersOf2UntilN(6);
             PowersOf2Until5();
-            PowersOf2Until5();
+
         }
 
         private static void PowersOf2Until5()
         {
-            for(int i = 1; i <= (1 << 5); i = i << 1)
+            PowersOf2UntilN(5);
+        }
+
+        private static void PowersOf2UntilN(int n)
+        {
+            for (int i = 1; i <= (1 << n); i = i << 1)
             {
                 Console.WriteLine(i);
             }
