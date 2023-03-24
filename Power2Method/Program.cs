cf5bf31fe7182e0cd6b99a5b9e1b70d8e0963c1d
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-24 16:00:36 +0000

    Add Overloading

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -12,6 +12,7 @@ namespace Power2Method
         {
             PowersOf2UntilN(7);
             PowersOf2UntilN(6);
+            PowersOf2UntilN(8,9);
             PowersOf2Until5();
 
         }
@@ -36,5 +37,18 @@ namespace Power2Method
             }
         }
 
+        /// <summary>
+        /// vai imprimir todos os numeros de 2 elevado a n entre n1 e n2
+        /// </summary>
+        /// <param name="n1">numero inicial a que o dois vai tar elevado</param>
+        /// <param name="n2">numero final a que o dois vai tar elevado</param>
+        private static void PowersOf2UntilN(int n1, int n2)
+        {
+            for (int i = (1 << n1); i <= (1 << n2); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        //&& (1 << n1) <= 
     }
 }
